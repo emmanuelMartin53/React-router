c3c2f5c1c8e9fd713781ad870275cff90a6c48c9
Emmanuel Martin <em.martin456@gmail.com>
2025-10-15 16:53:40 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,51 @@
 import Menu from "./components/Menu";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { Component } from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Docs from "./components/Docs";
+
 import Tutorial from "./components/Tutorial";
 import Community from "./components/Community";
 import ErrorPage from "./components/ErrorPage";
 import './App.css'
 
-function App() {
+// Redirect est remplacé par Navigate dans la version 6 de react-router-dom
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+       underConst: {
+        Docs: false,
+        Tutorial: true,
+        Community: false
+       }
+    }
+  }
+
+  render () {
+
+    // const {underConst} = this.state;
+
+    return (
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Docs />}/>
+          <Route path="/tutorial" element={<Tutorial />}/>
 
 
-  return (
-    <BrowserRouter>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Docs />}/>
-        <Route path="/tutorial" element={<Tutorial />}/>
-        <Route path="/community" element={<Community />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+          {/* <Route path="/tutorial" element={
+            underConst.Tutorial ? (<Navigate to="/" replace/>) : (<Tutorial />)
+          }/> */}
 
-
-
-    </BrowserRouter>
-  )
+          <Route path="/community" element={<Community />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App
--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -1,8 +1,20 @@
+import { Navigate } from "react-router-dom";
 
-const Tutorial = () => {
+const Tutorial = (props) => {
+  console.log(props)
+
+  setTimeout(() => {
+    window.location.href = "/"
+  }, 5000);
+
   return (
-    <div className="container">
+      <div className="container mt-3">
       <h1>Tutorial: Intro to react</h1>
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+          <strong>Oups</strong> this pages is under construction.
+          You will be redirected to the Docs page in 5 seconds
+          <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
     </div>
   )
 }
