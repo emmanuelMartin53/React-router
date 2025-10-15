11644512164f59003a90d51a89d8dd386eac29a0
Emmanuel Martin <em.martin456@gmail.com>
2025-10-15 11:06:24 +0200

    utilisation De NAvigate pour rediriger vers la page d'accueil

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
 import Menu from "./components/Menu";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { Component } from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Docs from "./components/Docs";
 import Tutorial from "./components/Tutorial";
 import Community from "./components/Community";
 import ErrorPage from "./components/ErrorPage";
 import './App.css'
 
-function App() {
+class App extends Component {
 
+  constructor(props) {
+    super(props)
 
-  return (
-    <BrowserRouter>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Docs />}/>
-        <Route path="/tutorial" element={<Tutorial />}/>
-        <Route path="/community" element={<Community />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+    this.state = {
+       underconst: {
+          Docs: false,
+          Tutorial: true,      // Page  encore en construction
+          Community: false
+       }
+    }
+  }
 
+  render () {
+    const {underconst} = this.state
 
+    return (
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Docs />}/>
+          <Route path="/tutorial" element={<Tutorial />}/>
 
-    </BrowserRouter>
-  )
+          {/* <Route path="/tutorial" element={
+            underconst.Tutorial ? (<Navigate to="/" />) : (<Tutorial />)
+          }/>
+           */}
+          <Route path="/community" element={<Community />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    )
+
+  }
+
 }
 
 export default App
--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -1,8 +1,22 @@
+import { Navigate } from "react-router-dom";
 
-const Tutorial = () => {
+
+const Tutorial = (props) => {
+  console.log(props)
+
+  setTimeout(() => {
+    window.location.href = "/" // méthode qui fonctionne avec react-router-dom v6
+  }, 5000);
+
+
   return (
-    <div className="container">
-      <h1>Tutorial: Intro to react</h1>
+      <div className="container mt-3">
+        <h1>Tutorial: Intro to react</h1>
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+          <strong>Oups</strong> this pages is under construction.
+          You will be redirected to the Docs page in 5 seconds
+          <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
     </div>
   )
 }
